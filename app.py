79a1d66c4d396c119bbe60e74bf0afce2222149e
Debian <bitnami@ip-172-26-0-95.eu-west-3.compute.internal>
2023-09-25 22:10:26 +0000

    Deploiement with AWS

--- app.py
+++ app.py
@@ -192,4 +192,4 @@ def edit_team(team_id):
 ################################################################################
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
